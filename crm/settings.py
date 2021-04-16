5a123525243926c692a2eb224a00610eb4b9258c
Дмитрий <dima.derkach2004@gmail.com>
2021-04-16 15:05:39 +0500

    'asd'

--- crm/settings.py
+++ crm/settings.py
@@ -77,6 +77,8 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
